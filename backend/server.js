ad0755633b6bf4a3e09c9c51bcab3848293730ad
maryamsaye <marynsaye@gmail.com>
2025-04-16 17:30:38 +0100

    Added backendfolder

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const flowersRouter = require('./routes/flowers'); 
+const mongoose = require('mongoose');
+require('dotenv').config();
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 4000; // ✅ Default value for PORT
+
+// Middleware to parse JSON
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// MongoDB connection string from .env
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error("MongoDB connection error:", err));
+
+//uploaded images statically
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+  app.get('/', (req, res) => {
+    res.send('Welcome to the Flower API!');
+  });
+  
+
+// Define a test route
+app.use('/api/flowers', flowersRouter); // ✅ Corrected variable name
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
