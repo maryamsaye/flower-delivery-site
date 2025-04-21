fa4fa22254b90e86f783512de0e0cdb836c0a9d1
maryamsaye <marynsaye@gmail.com>
2025-04-21 15:07:25 +0100

    Updated Readme file

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const userRouter = require('./routes/users');
 
 
 const app = express();
-const PORT = process.env.PORT || 4000; // ✅ Default value for PORT
+const PORT = process.env.PORT || 4000;
 
 // Middleware to parse JSON
 app.use(express.json());
